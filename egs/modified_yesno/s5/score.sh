62679b1d5478fc71582985ac358f8e3b32eabb96
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2021-04-02 01:54:51 +0100

    change dir in score.sh

--- egs/modified_yesno/s5/score.sh
+++ egs/modified_yesno/s5/score.sh
@@ -1,5 +1,5 @@
 exp=exp/mono_train_yyn_10
-pytorchdir=~/Documents/Work/CSTR/lennoxtown/s3/pytorch-kaldi/exp/modified_yesno_BLSTMP_mfcc_ynn
+pytorchdir=~/Documents/Work/CSTR/lennoxtown/s3/pytorch-kaldi/exp/modified_yesno_BLSTMP_mfcc_yyn_60_1
 
 for x in test_yyn test_ynn test_3gram test_sam_yyn test_sam_ynn test_sam_3gram test_sam_yyn_noise test_sam_ynn_noise test_sam_3gram_noise; do
     notest=${x#*_}
